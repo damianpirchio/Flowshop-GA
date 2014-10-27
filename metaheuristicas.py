74f68b47975e6b4e74a30e571ee0efc26cdd9c93
Damian Pirchio <damipirchio@gmail.com>
2014-10-27 17:43:37 -0300

    Ver que es

--- metaheuristicas.py
+++ metaheuristicas.py
@@ -22,8 +22,8 @@ class Poblacion(object):
 
     def mostrar(self):
         for c in self.cromosomas:
-            print (c.secuencia)
-            print (c.fitness)
+            print c.secuencia
+            print c.fitness
             print('----------------------')
 
 
@@ -81,30 +81,57 @@ class PBT(Selection):
 
 class Crossover(object):
     """docstring for Crossover"""
-    def __init__(self, arg):
+    def __init__(self):
         super(Crossover, self).__init__()
-        self.arg = arg
 
 
 class PMX(Crossover):
     """docstring for PMX"""
-    def __init__(self, arg):
+    def __init__(self):
         super(PMX, self).__init__()
-        self.arg = arg
 
     def cruzar(self, cromosoma1, cromosoma2):
+        lista_respuesta = []
         padre1_secuencia = cromosoma1.secuencia
+        print("padre1: " + str(padre1_secuencia))
         padre2_secuencia = cromosoma2.secuencia
+        print("padre2: " + str(padre2_secuencia))
         #Genero dos posiciones al azar teniendo en cuenta el tamano del cromo
-        pos1 = random.randint(0, cromosoma1.tamano)
-        pos2 = random.randint(0, cromosoma1.tamano)
+        #pos1 = random.randint(0, cromosoma1.tamano)
+        #pos2 = random.randint(0, cromosoma1.tamano)
+        pos1 = 2
+        pos2 = 6
         if pos1 >= pos2:
             pos_inicial = pos2
             pos_final = pos1
         else:
             pos_inicial = pos1
             pos_final = pos2
+        print("pos inicial: " + str(pos_inicial))
+        print("pos final: " + str(pos_final))
+        #Intercambio substrings entre los padres
+        #---------------------------------------
+        #1) Armo Substrings
+        substring1 = padre1_secuencia[pos_inicial:pos_final]
+        substring2 = padre2_secuencia[pos_inicial:pos_final]
+        print("Substring 1: " + str(substring1))
+        print("Substring 2: " + str(substring2))
+        #2)Armo Protochilds
+        padre1_secuencia[pos_inicial:pos_final] = substring2
+        padre2_secuencia[pos_inicial:pos_final] = substring1
+        print("protochild 1: " + str(padre1_secuencia))
+        print("protochild 2: " + str(padre2_secuencia))
+        #3)Armo Lista de Mapeo con los substrings
+        lista_mapeo = []
+        for i in range(len(substring1)):
+            lista_mapeo.append([substring1[i], substring2[i]])
+        print("lista de mapeo: " + str(lista_mapeo))
+        #4)Elimino Transitividad de la Lista de Mapeo
+        #5)Reemplazo Final con Lista De Mapeo
+        #for el in padre1_secuencia:
+            #if el not in substring1:
 
+        return lista_respuesta
 
 
 class CX(Crossover):
